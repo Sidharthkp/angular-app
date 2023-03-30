96bbb2ffb8287ab1f4d9cf5f17da22d83637b79e
Sidharthkp <sidharthkannan20@gmail.com>
2023-03-30 12:28:46 +0530

    working on delete task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
-import {Task} from '../../Task';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
@@ -10,9 +10,13 @@ import {Task} from '../../Task';
 export class TasksComponent {
   tasks: Task[] = [];
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.taskService.getTasks().subscribe((tasks)=>(this.tasks = tasks));
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id! === task.id)));
   }
 }
