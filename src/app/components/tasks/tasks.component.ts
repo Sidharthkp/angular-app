39dadadfa0dad4378b9a4606d8d84f742598113c
Sidharthkp <sidharthkannan20@gmail.com>
2023-03-30 12:32:50 +0530

    delete task completed

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -17,6 +17,6 @@ export class TasksComponent {
   }
 
   deleteTask(task: Task) {
-    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id! === task.id)));
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
   }
 }
